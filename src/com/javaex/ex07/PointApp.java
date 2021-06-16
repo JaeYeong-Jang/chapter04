8edcb7dee95312e9b88d9a0afacc29a93e546da2
jangjaeyeong <xkxh96@gmail.com>
2021-06-16 11:40:22 +0900

    06/16 coding(hashCode, equasl...)

--- src/com/javaex/ex07/PointApp.java
+++ src/com/javaex/ex07/PointApp.java
@@ -28,7 +28,25 @@ public class PointApp {
 		//중복체크 재정의 필요
 		Point p04 = new Point(3,6);
 		pSet.add(p04);
+		System.out.println("====");
 		System.out.println(pSet.toString());
+		System.out.println("====");
+		
+		
+		
+		//Point 비교
+		System.out.println(p02.equals(p04));
+		System.out.println(p02 == p04); //주소값 비교
+		
+		System.out.println(p01.equals(p04));
+		System.out.println(p01 == p04); // 변수 안의 값 비교 (주소)
+		
+		//hashcode 중복 되지 않는 숫자
+		System.out.println(p01.hashCode());
+		System.out.println(p02.hashCode());
+		System.out.println(p03.hashCode());
+		System.out.println(p04.hashCode());
+		
 		
 		
 	}
